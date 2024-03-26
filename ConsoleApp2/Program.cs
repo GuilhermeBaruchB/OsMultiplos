09d9485a0e1ce163c49650ad0fce8a852d2fef7a
Usuario <Usuario@CPD03-M10>
2024-03-26 10:47:57 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual numero voce deseja ver os múltiplos");
+            int numero = int.Parse(Console.ReadLine());
+
+
+            for (int i = numero; i <= 100; i += numero)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
